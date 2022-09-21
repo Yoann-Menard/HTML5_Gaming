2886d1f8624b25030f569c7d5563184bead25122
Yoann <yoann.menard@epitech.eu>
2022-09-21 10:31:07 +0200

    [master] -  converting the project to project oriented programming

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const config = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 800,
+  heigth: 600,
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: [Level1Scene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 800 },
+      debug: true,
+    },
+  }
+};
+
+const game = new Phaser.Game(config);
